2748da5b360568d371bf87a0e0ed3941477ce5f8
Mariusz Gronczewski <xani666@gmail.com>
2015-02-05 00:45:33 +0100

    generate and take events

--- src/plugin/clock/clock.go
+++ src/plugin/clock/clock.go
@@ -2,9 +2,33 @@ package clock
 
 import (
 	"plugin_interface"
+	"time"
+//	"fmt"
 )
 
 func New(config interface{}, events chan plugin_interface.Event, update chan plugin_interface.Update) {
+	for {
+		select {
+		case _ = (<-events):
+			Update(update)
+		case <-time.After(time.Second):
+			Update(update)
+		}
+	}
+
+}
+
+func Update(update chan plugin_interface.Update) {
+	time := GetTimeEvent(`2006-01-02 MST 15:04:05`)
+	time.Color=`#DDDDFF`
+	update <- time
+}
 
 
+func GetTimeEvent(format string) plugin_interface.Update {
+	t :=  time.Now().Local()
+	var ev plugin_interface.Update
+	ev.FullText = t.Format(format)
+	ev.ShortText = t.Format(`15:04:05`)
+	return ev
 }
