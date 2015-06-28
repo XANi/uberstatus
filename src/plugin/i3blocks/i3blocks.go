523a5f9c8ff0093497b48b64d8090e3b5d231036
Mariusz Gronczewski <xani666@gmail.com>
2015-06-28 17:21:25 +0200

    i3block plugin, examples

---
+++ src/plugin/i3blocks/i3blocks.go
@@ -0,0 +1,110 @@
+package i3blocks
+
+import (
+	"plugin_interface"
+	//	"gopkg.in/yaml.v1"
+	"os/exec"
+	"time"
+	"github.com/op/go-logging"
+	"strings"
+	"bytes"
+//	"fmt"
+)
+
+var log = logging.MustGetLogger("main")
+
+
+type Config struct {
+	prefix string
+	command string
+	interval int
+	color string
+}
+
+func New(config map[string]interface{}, events chan plugin_interface.Event, update chan plugin_interface.Update) {
+	c := loadConfig(config)
+	for {
+		select {
+		case _ = (<-events):
+			Update(update,c)
+		case <-time.After(time.Duration(c.interval) * time.Millisecond):
+			Update(update,c)
+		}
+	}
+
+}
+
+func loadConfig(raw map[string]interface{}) Config {
+	var c Config
+	c.interval = 1000
+	c.color = `#ffffff`
+	log.Info("loading config")
+	for key, value := range raw {
+		converted, ok := value.(string)
+		if ok {
+			switch {
+			case key == `prefix`:
+				c.prefix = converted
+			case key == `command`:
+				c.command = converted
+			case key == `color`:
+				c.color = converted
+			default:
+				log.Warning("unknown config key: [%s]", key)
+
+			}
+			log.Warning("t: %s %s", key, converted)
+		} else {
+			converted, ok := value.(int)
+			if ok {
+				switch {
+				case key == `interval`:
+					c.interval = converted
+				default:
+					log.Warning("unknown config key: [%s]", key)
+				}
+			} else {
+				log.Error("Cant interpret value of config key [%s]", key)
+			}
+		}
+	}
+	return c
+}
+
+
+func Update(update chan plugin_interface.Update, config Config) {
+	var ev plugin_interface.Update
+	cmd := exec.Command(config.command)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := out.String()
+
+    st := strings.Split(s, "\n")
+	st_len := len(st)
+
+	if st_len >= 4 {
+		ev.Color = st[2]
+	} else {
+		ev.Color = config.color
+	}
+
+	if st_len == 3 {
+		ev.ShortText = st[1]
+		ev.FullText = st[0]
+	}
+	if st_len == 2 {
+		ev.FullText = st[0]
+		ev.ShortText = st[0]
+	}
+	// len of 1 means there was nothing to split, no \n probably means invalid input
+	if st_len <= 1 {
+		log.Warning("Command %s returned nothing",config.command)
+		return
+	}
+	update <- ev
+}
