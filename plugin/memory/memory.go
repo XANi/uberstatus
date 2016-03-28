4e934d141194f7e336ad4651047c6cf4bdf0a4c9
Mariusz Gronczewski <xani666@gmail.com>
2016-03-28 08:19:27 +0200

    move bar before numbers

--- plugin/memory/memory.go
+++ plugin/memory/memory.go
@@ -69,13 +69,12 @@ func (state *state) updatePeriodic() uber.Update {
 		memFreePctForCalc = float64(memFree) / float64(memTotalForCalc) * 100
 	}
 	swapPct := 100 - ((mem.SwapFree * 100) / mem.SwapTotal)
-
 	update.FullText = fmt.Sprintf(`%s<span color="%s">%s</span><span color="%s">%s</span>`,
 		state.cfg.prefix,
-		util.GetColorPct(int(100-memFreePctForCalc)),
-		util.FormatUnitBytes(memFree),
 		util.GetColorPct(int(swapPct)),
 		util.GetBarChar(int(swapPct)),
+		util.GetColorPct(int(100-memFreePctForCalc)),
+		util.FormatUnitBytes(memFree),
 	)
 //		util.GetColorPct(
 	update.ShortText = fmt.Sprintf(`<span color="%s">%s%</span>`, 	util.GetColorPct(int(100-memFreePct)), memFreePct)
