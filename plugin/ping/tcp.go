bd3e18a6c7c16eca37f27d62ec6b3f847a0a720f
Mariusz Gronczewski <xani666@gmail.com>
2017-05-29 23:52:12 +0200

    port ping plugin; separate update and ping interval

--- plugin/ping/tcp.go
+++ plugin/ping/tcp.go
@@ -5,28 +5,18 @@ import (
 	"net"
 )
 
-func tcpPing(addr string, out chan *pingResult,t time.Duration) {
-	var okCount uint64
-	var failCount uint64
-	for {
-		var ping pingResult
-		timeStart := time.Now()
-		dial := net.Dialer{Timeout: time.Duration(time.Second * 10)}
-		c, err := dial.Dial("tcp", addr)
-		timeEnd := time.Now()
-		if err == nil {
-			c.Close()
-			okCount = okCount + 1
-			ping.Duration = timeEnd.Sub(timeStart)
-			ping.Ok = true
-		} else {
-			failCount = failCount + 1
-			ping.Ok = false
-		}
-		ping.OkCount = okCount
-		ping.FailCount = failCount
-		out <- &ping
-		time.Sleep(t)
+func tcpPing(addr string) pingResult {
+	var ping pingResult
+	timeStart := time.Now()
+	dial := net.Dialer{Timeout: time.Duration(time.Second * 10)}
+	c, err := dial.Dial("tcp", addr)
+	timeEnd := time.Now()
+	if err == nil {
+		c.Close()
+		ping.Duration = timeEnd.Sub(timeStart)
+		ping.Ok = true
+	} else {
+		ping.Ok = false
 	}
-
+	return ping
 }
