8df13550d7251f7903ef913764395765cadb57e1
Mariusz Gronczewski <xani666@gmail.com>
2020-07-09 18:51:41 +0200

    add timeout to http client

--- plugin/ping/http.go
+++ plugin/ping/http.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 )
 
+var pingHttpClient = http.Client{
+	Transport:     nil,
+	CheckRedirect: nil,
+	Jar:           nil,
+	Timeout:       120000,
+}
+
 func httpPing(addr string) pingResult {
 	var ping pingResult
 	timeStart := time.Now()
-	_, err := http.Head(addr)
+	_, err := pingHttpClient.Head(addr)
 	timeEnd := time.Now()
 	if err == nil {
 		ping.Duration = timeEnd.Sub(timeStart)
